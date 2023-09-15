5447f5c0ef8b9193fcf4f4e5eff75a5d35797eb0
Yana test <yana_ivanchenkoo@mail.ru>
2023-09-15 17:13:26 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 10803;
+        int mile = 20;
+        int bonus = price / mile;
+
+        System.out.println("Количество начисленных миль: " + bonus);
+    }
+}
